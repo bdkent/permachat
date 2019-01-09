33cbeb9d0ca029ea41d30beacb2482dbd1027bc4
Brian Kent <brian.kent@basketsavings.com>
2019-01-09 11:12:22 -0500

    indexer reconnection attempts

--- client/src/indexer.js
+++ client/src/indexer.js
@@ -1,5 +1,7 @@
 import _ from "lodash";
 
+import fs from "fs";
+
 import PermaChatContract from "./contracts/PermaChat.json";
 import truffleContract from "truffle-contract";
 import Web3 from "web3";
@@ -11,36 +13,101 @@ import IndexerContractService from "./indexer/IndexerContractService";
 import ScheduledIndexer from "./indexer/ScheduledIndexer";
 import IndexPricer from "./indexer/IndexPricer";
 
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
+
+const getSecret = name => {
+  const dockerSecretPath = "/run/secrets/" + name;
+  if (fs.existsSync(dockerSecretPath)) {
+    return _.trim(fs.readFileSync(dockerSecretPath, "utf8"));
+  }
+  if (_.has(process.env, [name])) {
+    return _.trim(process.env[name]);
+  }
+  logger.error("cannot find secret", name);
+  return null;
+};
 
 const newNetworkConfig = networkType => {
-  switch (networkType) {
-    case "private":
-    case "ganache":
-      return {
-        provider: new Web3.providers.WebsocketProvider("ws://127.0.0.1:7545")
-      };
-    case "rinkeby":
-      return {
-        // provider: new Web3.providers.WebsocketProvider(
-        //   "wss://rinkeby.infura.io/ws"
-        // )
-        // ,
-        // WSS ???
-        provider: new Web3.providers.WebsocketProvider("ws://127.0.0.1:8546"),
-        networkAccount: process.env.PERMACHAT_ACCOUNT,
-        password: process.env.PERMACHAT_PASSWORD
-      };
-    default:
-      throw new Error("unknown network: " + networkType);
+  logger.debug("newNetworkConfig", networkType);
+  try {
+    switch (networkType) {
+      case "private":
+      case "ganache":
+        return {
+          provider: new Web3.providers.WebsocketProvider("ws://127.0.0.1:7545")
+        };
+      case "rinkeby":
+        return {
+          // provider: new Web3.providers.WebsocketProvider(
+          //   "wss://rinkeby.infura.io/ws"
+          // )
+          // ,
+          // WSS ???
+          provider: new Web3.providers.WebsocketProvider("ws://127.0.0.1:8546"),
+          networkAccount: getSecret("PERMACHAT_ACCOUNT"),
+          password: getSecret("PERMACHAT_PASSWORD")
+        };
+      default:
+        throw new Error("unknown network: " + networkType);
+    }
+  } catch (e) {
+    console.error("unable to create new network config", e);
+    throw new Error("error: " + networkType);
   }
+};
+
+const newWeb3 = provider => {
+  return new Promise((resolve, reject) => {
+    var count = 0;
+
+    const MaxRetryAttempts = 50;
+    const ConnectIntervalMs = 1000 * 5;
+
+    const tryCreate = async () => {
+      logger.debug("web3 creation attempt", ++count);
+      try {
+        const web3 = new Web3(provider);
+        if (_.isNil(web3)) {
+          return Promise.reject("unable to create web3");
+        } else {
+          const isListening = await web3.eth.net.isListening();
+          if (isListening) {
+            logger.info("web3 is listening successfully");
+            return Promise.resolve(web3);
+          } else {
+            return Promise.reject("unable to connect with web3");
+          }
+        }
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    };
+
+    const attempt = () => {
+      tryCreate()
+        .then(resolve)
+        .catch(e => {
+          if (count < MaxRetryAttempts) {
+            setTimeout(attempt, ConnectIntervalMs);
+          } else {
+            reject(
+              "count not create Web3 connections, after " + count + " attempts"
+            );
+          }
+        });
+    };
+
+    attempt();
+  });
 };
 
 const config = async networkType => {
   const { provider, networkAccount, password } = newNetworkConfig(networkType);
+  console.log("provider", !_.isNil(provider), networkAccount);
 
-  const web3 = new Web3(provider);
+  const web3 = await newWeb3(provider);
+  console.log("web3", !_.isNil(web3));
 
   const network = await web3.eth.net.getNetworkType();
   console.log("NETWORK", network);
