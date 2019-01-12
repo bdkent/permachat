282e5d111a688db0916e16d8d8d854a550659429
Brian Kent <brian.kent@basketsavings.com>
2019-01-12 00:30:30 -0500

    publish to ipfs script

--- client/publish-to-ipfs.sh
+++ client/publish-to-ipfs.sh
@@ -2,8 +2,11 @@
 
 #curl -F file=@build "https://ipfs.infura.io:5001/api/v0/add?recursive=true"
 
+npm run build
+
 curl "https://ipfs.infura.io:5001/api/v0/add?recursive=true" \
     --verbose \
     -X POST \
+    -o ipfs.out \
     -H "Content-Type: multipart/form-data" \
-    -F 'file=@"stuff"'
+    -F 'file=@"build"'
