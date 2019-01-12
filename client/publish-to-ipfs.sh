b386ebb1f9547b6d7ea56d30023dc298fc1dba7d
Brian Kent <brian.kent@basketsavings.com>
2019-01-12 01:39:05 -0500

    useless

--- client/publish-to-ipfs.sh
+++ client/publish-to-ipfs.sh
@@ -2,11 +2,10 @@
 
 #curl -F file=@build "https://ipfs.infura.io:5001/api/v0/add?recursive=true"
 
+cd ${HOME}/git/permachat/client
+
 npm run build
 
 curl "https://ipfs.infura.io:5001/api/v0/add?recursive=true" \
-    --verbose \
-    -X POST \
-    -o ipfs.out \
     -H "Content-Type: multipart/form-data" \
     -F 'file=@"build"'
