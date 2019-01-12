380c94ad9470d9b8cf0c09909056b14348f8d107
Brian Kent <brian.kent@basketsavings.com>
2019-01-12 01:39:17 -0500

    docker

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,17 @@
 
 # env vars???    -e x=y
 # docker-compose up -d --force-recreate --remove-orphans 
+
+
+docker build --no-cache --tag=indexer-service ${HOME}/git/permachat/
+docker tag indexer-service bkent/permachat-indexer-service
+docker login
+docker push bkent/permachat-indexer-service
+
+docker build --no-cache --tag=identity-service ${HOME}/go/src/github.com/bdkent/permachat-services/internal/app/identity
+docker tag identity-service bkent/permachat-identity-service
+docker login
+docker push bkent/permachat-identity-service
+
+docker-compose up -d --force-recreate --remove-orphans -e GETH_NETWORK=--rinkeby
+  
