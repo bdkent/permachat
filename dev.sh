afb5c9632e214cdb574af048a750607bd7009a3a
Brian Kent <brian.kent@basketsavings.com>
2019-01-17 22:35:44 -0500

    moving to folder

--- dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker container stop ganache-local
-docker container rm ganache-local
-docker run -d                    \
-           --name ganache-local  \
-           -p 7545:7545          \
-           trufflesuite/ganache-cli:v6.2.5  -p 7545 \
-                                            -m stumble scissors finger melt market master spray intact audit notable excuse elephant
